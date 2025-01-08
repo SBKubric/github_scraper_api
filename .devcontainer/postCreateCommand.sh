042b141d3593085edf52569091aa73cae426fb5b
sbkubric <sbkubric@gmail.com>
2025-01-08 05:26:22 +0000

    fix: git submodule init fix

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+git rm -rf .git
+git init
+git remote add origin git@github.com:SBKubric/github_scraper_api.git
+git fetch origin
+git checkout --force main
+
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.cargo/env
